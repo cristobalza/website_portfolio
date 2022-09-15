ebc4f6b22977d25ecf907f30b14b95d8e571a198
cristobalza <cristobalzamas@gmail.com>
2022-09-15 20:40:59 -0300

    changed some colors

--- js/app.js
+++ js/app.js
@@ -14,3 +14,15 @@ hiddenElements1.forEach((el) => observer.observe(el));
 
 const hiddenElements2 = document.querySelectorAll('.hiddenRightToLeft');
 hiddenElements2.forEach((el) => observer.observe(el));
+
+const tween = KUTE.fromTo(
+    '#blob1', {
+        path: '#blob1'
+    }, {
+        path: '#blob2'
+    }, {
+        repeat: 999,
+        duration: 3000,
+        yoyo: true
+    }
+).start();
